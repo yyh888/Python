613c52b4c1020883aa2ac5a95924200efe514b08
yyh888 <1161752748@qq.com>
2023-11-19 22:15:54 +0800

    01

---
+++ python01/practice.py
@@ -0,0 +1,66 @@
+# a = 1
+# print(type(a)) # <class 'int'>
+
+# a = 1.01
+# print(type(a)) # <class 'float'>
+
+# a = 'abc'
+# print(type(a))
+
+# a = "my name is 'yyh'"
+# b = 'my name is "yyh"'
+
+# a = '''My 'name' is "yyh"'''
+# print(a) # My 'name' is "yyh"
+
+# a = 'abcdef'
+# b = 'ghi'
+# c = a + b
+# print(c) # abcdefghi
+
+# a = True
+# print(a) # True
+
+# a = 10
+# b = 20
+# print(a == b) # False
+
+"""
+a = 10
+print(type(a)) # <class 'int'>
+a = '10'
+print(type(a)) # <class 'str'>
+"""
+
+'''
+这是一个注释
+'''
+
+# a = 100
+# print(a) # 100
+
+
+# a = 100
+# print(f"a = {a}") # a = 100
+
+
+# a = input('请输入第一个数字: ')
+# b = input('请输入第二个数字: ')
+# a = int(a)
+# b = int(b)
+# print(f"a + b = {a + b}")
+
+
+# print(0.1 + 0.2 == 0.3) # False
+# print(0.1 + 0.2) # 0.30000000000000004
+
+# a = 100
+# b = 200
+# print(not a > b) # True
+
+# a = b = 100
+# print(f"a = {a}, b = {b}") # a = 100, b = 100
+
+a, b = 100, 200
+a, b = b, a
+print(a, b) # 200 100
